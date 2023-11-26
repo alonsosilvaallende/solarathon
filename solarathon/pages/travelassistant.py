cb354f96ce8a2b55ff09dd029da9126cc65da1d6
Alonso Silva <alonso.silva@gmail.com>
2023-11-26 20:11:02 +0100

    Add ipyleaflet map

--- solarathon/pages/travelassistant.py
+++ solarathon/pages/travelassistant.py
@@ -1,5 +1,10 @@
 import solara
 import datetime as dt
+import ipyleaflet
+
+zoom = solara.reactive(10)
+center = solara.reactive((48.8566, 2.3522))
+bounds = solara.reactive(None)
 
 @solara.component
 def Page():
@@ -8,3 +13,16 @@ def Page():
     
     #solara.Text(str(dates.value))
     solara.Text("You are travelling from " + str(dates.value[0].strftime("%A, %d %B %Y")) + " to " + str(dates.value[1].strftime("%A, %d %B %Y"))+".")
+    with solara.Column(style={"max-width": "500px", "height": "500px"}):
+        solara.SliderInt(label="Zoom level", value=zoom, min=1, max=20)
+        ipyleaflet.Map.element(  # type: ignore
+                zoom=zoom.value,
+                on_zoom=zoom.set,
+                center=center.value,
+                on_center=center.set,
+                on_bounds=bounds.set,
+                scroll_wheel_zoom=True,
+            )
+        solara.Text(f"Zoom: {zoom.value}")
+        solara.Text(f"Center: {center.value}")
+        solara.Text(f"Bounds: {bounds.value}")
